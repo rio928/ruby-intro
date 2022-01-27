6eb63f510371a39d0e5a1809392bfe19ac0423bc
rio928 <rionex928@yahoo.co.jp>
2022-01-27 04:25:29 +0000

    answered

--- labs/1-data.rb
+++ labs/1-data.rb
@@ -20,3 +20,9 @@
 # "i would like " + 5 + " tacos"
 # To rectify this, you can convert the number into a string
 # "i would like #{5} tacos"
+
+dice1 = rand(1..6)
+dice2 = rand(1..6)
+puts "dice1 = #{dice1}"
+puts "dice2 = #{dice2}"
+puts dice1 + dice2
